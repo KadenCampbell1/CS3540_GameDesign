faa34d09ce98db9504b87b20b1c848ca6742bee9
Kaden(LenLap) <10836246@uvu.edu>
2022-11-14 09:20:19 -0700

    Updated Turret rotation; removed AI Controller; Hid puzzle block

--- Platformer_Final/Assets/_Scripts/Turret.cs
+++ Platformer_Final/Assets/_Scripts/Turret.cs
@@ -29,7 +29,7 @@ public class Turret : MonoBehaviour
         Vector3 dir = target.position - transform.position;
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
-        transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+        transform.rotation = Quaternion.Euler(rotation.x, rotation.y, 0f);
 
         if (fireCountDown <= 0f)
         {
